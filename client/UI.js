ac41449aacf384a4dbef6e546c41c040cc7480fb
Dean Radcliffe <deanmisc@yahoo.com>
2017-02-23 00:19:40 -0600

    WIP

--- client/UI.js
+++ client/UI.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { mount } from 'react-mounter'
 import { store, announce } from '/imports/antares/main'
 
-const game = ({ drawIsVisible, drawIsMine, offerDraw, currentPlayer }) => (
+const game = ({ drawIsVisible, drawConcluded, drawStatus, drawIsMine, offerDraw, currentPlayer }) => (
     <div>
         <h3>The Game...</h3>
                 <div className="sm">
@@ -27,11 +27,14 @@ const game = ({ drawIsVisible, drawIsMine, offerDraw, currentPlayer }) => (
                         <div>
                             Your opponent offers you a draw.
                             <ul>
-                                <li><a href="#change-sides" onClick={(e) => {
+                                <li><a href="#draw-accept" onClick={(e) => {
                                     announce(Actions.Draw.reply, { accept: true, player: currentPlayer })
                                     e.preventDefault()
                                 }}>Accept</a></li>
-                                <li><a href>Decline</a></li>
+                                <li><a href="#draw-decline" onClick={(e) => {
+                                    announce(Actions.Draw.reply, { accept: false, player: currentPlayer })
+                                    e.preventDefault()
+                                }}>Decline</a></li>
                             </ul>
                         </div>
                 }
@@ -41,6 +44,12 @@ const game = ({ drawIsVisible, drawIsMine, offerDraw, currentPlayer }) => (
             !drawIsVisible() &&
             <div className="draw-offer">
                 <button onClick={offerDraw}>Offer Draw</button>
+            </div>
+        }
+        {
+            drawConcluded() &&
+            <div className="draw-status">
+                Draw Staus: { drawStatus() }
             </div>
         }
     </div>
@@ -54,20 +63,33 @@ const mapStateToProps = state => {
     let gameId = getCurrentGameId()
     let game = state.antares.get(gameId)
 
+    // The props will be an aggregation of the following field types:
+    // - View state
+    // - Persisted data state
+    // - Event Handlers
     return {
+        // View fields
+        currentPlayer: state.view.get('currentPlayer'),
+        // Accessors/Helpers for persisted data 
         drawIsVisible: () => {
             return game && game.get('draw') && game.getIn(['draw', 'status']) !== 'accepted'
+        },
+        drawConcluded: () => {
+            return ['accepted', 'declined'].includes(game && game.getIn(['draw', 'status']))
+        },
+        drawStatus: () => {
+            return game.getIn(['draw', 'status'])
         },
         drawIsMine: () => {
             return game && game.getIn(['draw', 'offeredBy']) === currentPlayer
         },
-        // Event handlers
+        // Event handlers - announcement creators
         offerDraw: () => {
             announce(Actions.Draw.offer, { player: currentPlayer })
                 .endOfEpic()
+                // This is how you attach behavior to the end of an epic that an action triggers
                 .then(() => console.log('The Draw Epic Is Finito.'))
-        },
-        currentPlayer
+        }
     }
 }
 
